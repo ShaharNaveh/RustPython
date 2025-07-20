a8036dd1bab97ca1cbe2c4506212ccdf486d0f98
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2025-07-20 11:35:41 +0200

    Update `token{ize,}.py` from 3.13.5

---
+++ grrr.py
@@ -0,0 +1,47 @@
+import _tokenize
+import io
+
+def meep():
+    return 123
+
+b = io.StringIO("""
+for i in range(10):
+
+
+
+
+  pass
+""".lstrip()
+)
+
+b = io.StringIO("""
+for i in range(10):\n pass
+""".strip()
+)
+
+#b = io.StringIO(None)
+
+it = _tokenize.TokenizerIter(b.readline, extra_tokens=False)
+#it = _tokenize.TokenizerIter(meep, extra_tokens=False)
+for i in it:
+  print(i)
+
+exit()
+
+print()
+print()
+print()
+
+
+b = io.StringIO("""
+for i in range(10):
+""".lstrip()
+)
+
+#b = io.StringIO(None)
+
+it = _tokenize.TokenizerIter(b.readline, extra_tokens=True)
+#it = _tokenize.TokenizerIter(meep, extra_tokens=False)
+for i in it:
+  print(i)
+
