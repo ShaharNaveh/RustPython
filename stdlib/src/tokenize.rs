20e3943a00408668614fb32d01b81350094225d5
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2025-07-23 19:06:47 +0200

    save cast to strwq

--- stdlib/src/tokenize.rs
+++ stdlib/src/tokenize.rs
@@ -10,29 +10,40 @@ mod _tokenize {
             PyResult,
             VirtualMachine,
             // PyObjectRef,
-            builtins::PyTypeRef,
+            builtins::{PyStr, PyTypeRef},
             function::ArgCallable,
             protocol::PyIterReturn,
             types::{Constructor, IterNext, Iterable, SelfIter},
         },
     };
-
-    //#[derive(Debug, PyPayload)]
     use ruff_python_trivia::SimpleTokenizer;
+    use std::fmt;
 
     #[pyattr]
-    #[pyclass(name = "TokenizerIter", module = "_tokenize")]
-    pub struct PyTokenizerIter<'a> {
+    #[pyclass(name = "TokenizerIter")]
+    #[derive(PyPayload)]
+    pub struct PyTokenizerIter {
         readline: ArgCallable,
         extra_tokens: bool,
         encoding: String,
-        state: PyRwLock<PyTokenizerIterState<'a>>,
+        state: PyRwLock<PyTokenizerIterState>,
+    }
+
+    impl fmt::Debug for PyTokenizerIter {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            f.debug_struct("PyTokenizerIter")
+                .field("readline", &self.readline)
+                .field("extra_tokens", &self.extra_tokens)
+                .field("encoding", &self.encoding)
+                //.field("state", &self.state)
+                .finish()
+        }
     }
 
     #[pyclass(with(Constructor, Iterable, IterNext))]
-    impl PyTokenizerIter<'_> {}
+    impl PyTokenizerIter {}
 
-    impl<'_> Constructor for PyTokenizerIter<'_> {
+    impl Constructor for PyTokenizerIter {
         type Args = PyTokenizerIterArgs;
 
         fn py_new(cls: PyTypeRef, args: Self::Args, vm: &VirtualMachine) -> PyResult {
@@ -52,12 +63,31 @@ mod _tokenize {
         }
     }
 
-    impl<'a> SelfIter for PyTokenizerIter<'a> {}
+    impl SelfIter for PyTokenizerIter {}
 
-    impl<'a> IterNext for PyTokenizerIter<'a> {
+    impl IterNext for PyTokenizerIter {
         fn next(zelf: &Py<Self>, vm: &VirtualMachine) -> PyResult<PyIterReturn> {
-            println!("{:#?}", zelf.readline.invoke((), vm));
+            let state = zelf.state.read();
+            let last_line = match &state.last_line {
+                Some(line) => line,
+                None => &zelf
+                    .readline
+                    .invoke((), vm)?
+                    .downcast::<PyStr>()
+                    .map_err(|_| vm.new_type_error("readline() returned a non-string object"))?,
+            };
 
+            dbg!("{:?}", last_line);
+            /*
+            let state = {
+                let state_guard = zelf.state.read();
+                state_guard.clone()
+            };
+            */
+
+            //  println!("State: {:?}", state.last_line);
+
+            // NOTE: in future if we read EOL then set `last_line` to None
             return Ok(PyIterReturn::StopIteration(None));
         }
     }
@@ -74,10 +104,10 @@ mod _tokenize {
         encoding: String,
     }
 
-    #[derive(Debug, Default)]
-    pub struct PyTokenizerIterState<'a> {
-        tokenizer: Option<SimpleTokenizer<'a>>,
-        last_line: Option<&'a str>,
+    #[derive(Default)]
+    pub struct PyTokenizerIterState {
+        tokenizer: Option<SimpleTokenizer<'static>>,
+        last_line: Option<PyStr>,
         last_lineno: u32,
         last_end_lineno: u32,
         byte_col_offset_diff: Option<u32>,
