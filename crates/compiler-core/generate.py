5f7b702c3e2ff77198c3945fce11ab7797d15c6e
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2026-02-18 10:55:03 +0200

    Base TryFrom

--- crates/compiler-core/generate.py
+++ crates/compiler-core/generate.py
@@ -23,7 +23,7 @@ def rustfmt(code: str) -> str:
     return subprocess.check_output(["rustfmt", "--emit=stdout"], input=code, text=True)
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True, slots=True)
 class Instr:
     _inner: "analyzer.Instruction | analyzer.PseudoInstruction"
 
@@ -31,11 +31,15 @@ class Instr:
     def name(self) -> str:
         return self._inner.name.title().replace("_", "")
 
+    @property
+    def opcode(self) -> int:
+        return self._inner.opcode
+
     def __lt__(self, other) -> bool:
         return self._inner.opcode < other._inner.opcode
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True, slots=True)
 class OpcodeMeta(metaclass=abc.ABCMeta):
     _analysis: analyzer.Analysis
 
@@ -43,8 +47,11 @@ class OpcodeMeta(metaclass=abc.ABCMeta):
     def __iter__(self) -> "Iterator[Instr]": ...
 
     @property
+    def enum_name(self) -> str:
+        return type(self).__name__
+
+    @property
     def rust_code(self) -> str:
-        enum_name = type(self).__name__
         variants = ",\n".join(instr.name for instr in self)
 
         methods = "\n\n".join(
@@ -60,18 +67,38 @@ class OpcodeMeta(metaclass=abc.ABCMeta):
 
         return f"""
         #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-        pub enum {enum_name} {{
+        pub enum {self.enum_name} {{
             {variants}
         }}
 
-        impl {enum_name} {{
+        impl {self.enum_name} {{
             {methods}
         }}
 
         {impls}
         """.strip()
 
+    @property
+    def impl_tryfrom_numeric(self) -> str:
+        num_repr = "u8" if next(iter(self)).opcode < 255 else "u16"
+        arms = ",\n".join(f"{instr.opcode} => Self::{instr.name}" for instr in self)
 
+        return f"""
+        impl TryFrom<{num_repr}> for {self.enum_name} {{
+            type Error = crate::marshal::MarshalError;
+
+            fn try_from(value: {num_repr}) -> Result<Self, Self::Error> {{
+                Ok(match value {{
+                    {arms},
+                    _ => return Err(Self::Error::InvalidBytecode)
+                }}
+                )
+            }}
+        }}
+        """
+
+
+@dataclasses.dataclass(frozen=True, slots=True)
 class Opcode(OpcodeMeta):
     def __iter__(self) -> "Iterator[Instr]":
         yield from sorted(map(Instr, self._analysis.instructions.values()))
