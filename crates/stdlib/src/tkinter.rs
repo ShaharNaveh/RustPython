018819cf19ec16b92ee889a31544764597a71770
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2026-05-05 12:40:29 +0300

    fix tk

--- crates/stdlib/src/tkinter.rs
+++ crates/stdlib/src/tkinter.rs
@@ -154,8 +154,9 @@ mod _tkinter {
         // }
 
         // str
-        if let Some(str) = obj.downcast_ref::<PyStr>() {
-            return Ok(str.as_str().to_string());
+
+        if let Some(varname) = obj.downcast_ref::<PyStr>().map(|s| s.to_string()) {
+            return Ok(varname);
         }
 
         if let Some(_tcl_obj) = obj.downcast_ref::<TclObject>() {
@@ -167,7 +168,7 @@ mod _tkinter {
         // Construct an error message using the type name (truncated to 50 characters).
         Err(vm.new_type_error(format!(
             "must be str, bytes or Tcl_Obj, not {:.50}",
-            obj.obj_type().str(vm)?.as_str()
+            obj.class().name(),
         )))
     }
 
